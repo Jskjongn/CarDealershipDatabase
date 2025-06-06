74e96294ea7f5ac655722429f4b80d60d6acf947
Jack Skjong-Nilsen <jacksnilsen@gmail.com>
2025-06-06 11:42:20 -0500

    Add vehicles table

--- CarDealership.sql
+++ CarDealership.sql
@@ -23,3 +23,20 @@ CREATE TABLE `dealerships` (
     `Phone` VARCHAR(12) NOT NULL,
     CONSTRAINT `PK_Dealership_ID` PRIMARY KEY (`Dealership_ID`)
 );
+
+# ---------------------------------------------------------------------- #
+# Add table "vehicles"                                                   #
+# ---------------------------------------------------------------------- #
+
+CREATE TABLE `vehicles` (
+	`VIN` VARCHAR(17) NOT NULL,
+    `Year` INTEGER NOT NULL,
+    `Make` VARCHAR(15) NOT NULL,
+    `Model` VARCHAR(15) NOT NULL,
+    `Vehicle_Type` VARCHAR(10) NOT NULL,
+    `Color` VARCHAR(10) NOT NULL,
+    `Odometer` INTEGER NOT NULL,
+    `Price` DECIMAL(10,2) NOT NULL,
+    `SOLD` ENUM('YES', 'NO') DEFAULT NULL,
+    CONSTRAINT `PK_VIN` PRIMARY KEY (`VIN`)
+);
