203a318e5369a12b70c925607baa2a76aa5e459d
Jack Skjong-Nilsen <jacksnilsen@gmail.com>
2025-06-06 14:10:13 -0500

    Create lease_contracts table

--- CarDealership.sql
+++ CarDealership.sql
@@ -37,7 +37,7 @@ CREATE TABLE `vehicles` (
     `Color` VARCHAR(10) NOT NULL,
     `Odometer` INTEGER NOT NULL,
     `Price` DECIMAL(10,2) NOT NULL,
-    `SOLD` ENUM('YES', 'NO') DEFAULT NULL,
+    `SOLD` ENUM('SOLD', 'NOT SOLD') DEFAULT 'NOT SOLD',
     CONSTRAINT `PK_VIN` PRIMARY KEY (`VIN`)
 );
 
@@ -58,15 +58,33 @@ CREATE TABLE `inventory` (
 
 CREATE TABLE `sales_contracts` (
 	`Sales_Contract_ID` INTEGER NOT NULL AUTO_INCREMENT,
-	`Date` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+	`Date` DATE,
     `Customer_Name` VARCHAR(50) NOT NULL,
     `Customer_Email` VARCHAR(20) NOT NULL,
     `VIN` VARCHAR(17) NOT NULL,
     `Sales_Tax` DECIMAL(10,2) NOT NULL,
     `Recording_Fee` INTEGER DEFAULT 100 NOT NULL,
     `Processing_Fee` DECIMAL(10,2) NOT NULL,
-    `Financed` ENUM('YES', 'NO') DEFAULT NULL,
-    `Monthly_Payment` DECIMAL(10,2) DEFAULT NULL,
+    `Financed` ENUM('YES', 'NO') DEFAULT 'NO',
+    `Monthly_Payment` DECIMAL(10,2) NOT NULL,
     CONSTRAINT `PK_Sales_Contract_ID` PRIMARY KEY (`Sales_Contract_ID`),
+    CONSTRAINT `FK_VIN` FOREIGN KEY (`VIN`) REFERENCES `vehicles` (`VIN`)
+);
+
+# ---------------------------------------------------------------------- #
+# Add table "lease_contracts"                                            #
+# ---------------------------------------------------------------------- #
+
+CREATE TABLE `lease_contracts` (
+	`Lease_Contract_ID` INTEGER NOT NULL AUTO_INCREMENT,
+	`Date` DATE,
+    `Customer_Name` VARCHAR(50) NOT NULL,
+    `Customer_Email` VARCHAR(20) NOT NULL,
+    `VIN` VARCHAR(17) NOT NULL,
+    `Ending_Value` DECIMAL(10,2) NOT NULL,
+    `Lease_Fee` DECIMAL(10,2) NOT NULL,
+    `Financed` ENUM('YES', 'NO') DEFAULT 'NO',
+    `Monthly_Payment` DECIMAL(10,2) NOT NULL,
+    CONSTRAINT `PK_Lease_Contract_ID` PRIMARY KEY (`Lease_Contract_ID`),
     CONSTRAINT `FK_VIN` FOREIGN KEY (`VIN`) REFERENCES `vehicles` (`VIN`)
 );
